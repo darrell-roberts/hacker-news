3ee2c86cd404bb609fbe56e9622c17e075bb4d8d
Darrell Roberts <33698065+darrell-roberts@users.noreply.github.com>
2023-12-29 12:31:46 -0500

    Disable zoom input when search is active (#78)

--- hacker-news-egui/src/renderer.rs
+++ hacker-news-egui/src/renderer.rs
@@ -31,12 +31,14 @@ pub fn render<'a>(
     }
 
     context.input(|input| {
-        if input.key_pressed(Key::Minus) {
-            app_state.emit(Event::ZoomOut);
-        }
+        if !app_state.search_open {
+            if input.key_pressed(Key::Minus) {
+                app_state.emit(Event::ZoomOut);
+            }
 
-        if input.key_pressed(Key::PlusEquals) {
-            app_state.emit(Event::ZoomIn);
+            if input.key_pressed(Key::PlusEquals) {
+                app_state.emit(Event::ZoomIn);
+            }
         }
     });
 
