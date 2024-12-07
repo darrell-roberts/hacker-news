6a411d962a4bdaf967b58aeb1dc9bcf58eafa12d
Darrell Roberts <droberts0674@gmail.com>
2024-12-07 10:18:52 -0500

    update search prototype

--- hacker-news-search/src/main.rs
+++ hacker-news-search/src/main.rs
@@ -1,6 +1,10 @@
 use hacker_news_search::{index, SearchContext, ITEM_RANK};
 use std::{fs::exists, path::Path};
-use tantivy::{collector::TopDocs, Document, Order, TantivyDocument};
+use tantivy::{
+    collector::TopDocs,
+    schema::{OwnedValue, Value},
+    Document, Order, TantivyDocument,
+};
 use tokio::fs::{create_dir_all, remove_dir_all};
 
 const INDEX_PATH: &str = "/tmp/hacker-news";
@@ -29,12 +33,25 @@ fn search() -> anyhow::Result<()> {
 
     let query = ctx.query()?.parse_query("category:top AND type:story")?;
 
-    let top_docs = TopDocs::with_limit(25).order_by_u64_field(ITEM_RANK, Order::Asc);
+    let top_docs = TopDocs::with_limit(10)
+        // Pagination
+        .and_offset(10)
+        // Ordering
+        .order_by_u64_field(ITEM_RANK, Order::Asc);
     let docs = searcher.search(&query, &top_docs)?;
 
     for (_score, doc_address) in docs {
         let retrieved_doc: TantivyDocument = searcher.doc(doc_address)?;
-        println!("{}", retrieved_doc.to_json(&ctx.schema));
+        for (field, field_values) in retrieved_doc.get_sorted_field_values() {
+            let field_name = ctx.schema.get_field_name(field);
+
+            let values: Vec<OwnedValue> = field_values
+                .into_iter()
+                .map(|val| val.as_value().into())
+                .collect();
+            print!("{field_name}: {values:?} ");
+        }
+        println!();
     }
 
     Ok(())
