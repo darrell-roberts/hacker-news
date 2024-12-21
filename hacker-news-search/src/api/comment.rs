8d4d4be40e92437f69a2f40cc277125a35ca0cac
Darrell Roberts <droberts0674@gmail.com>
2024-12-21 09:01:18 -0500

    use query parser

--- hacker-news-search/src/api/comment.rs
+++ hacker-news-search/src/api/comment.rs
@@ -1,13 +1,10 @@
 //! Search API for user comments.
-use std::time::SystemTime;
-
 use super::{Comment, Story};
-use crate::{
-    SearchContext, SearchError, ITEM_BODY, ITEM_ID, ITEM_RANK, ITEM_STORY_ID, ITEM_TIME, ITEM_TYPE,
-};
+use crate::{SearchContext, SearchError, ITEM_ID, ITEM_RANK, ITEM_STORY_ID, ITEM_TIME, ITEM_TYPE};
+use std::time::SystemTime;
 use tantivy::{
     collector::{Count, MultiCollector, TopDocs},
-    query::{BooleanQuery, FuzzyTermQuery, Occur, Query, RangeQuery, TermQuery},
+    query::{BooleanQuery, Occur, Query, RangeQuery, TermQuery},
     schema::IndexRecordOption,
     Order, Searcher, TantivyDocument, Term,
 };
@@ -24,14 +21,7 @@ impl SearchContext {
             .query_parser()?
             .parse_query(&format!("parent_id:{parent_id}"))?;
 
-        // self.top_comments_with_count(
-        //     limit,
-        //     offset,
-        //     query,
-        //     Some(|top_docs: TopDocs| Box::new(top_docs.order_by_u64_field(ITEM_RANK, Order::Asc))),
-        // )
         let searcher = self.searcher();
-
         let mut multi_collector = MultiCollector::new();
 
         let top_docs = TopDocs::with_limit(limit)
@@ -138,27 +128,10 @@ impl SearchContext {
             IndexRecordOption::Basic,
         ));
 
-        let fuzzy_term = Box::new(FuzzyTermQuery::new(
-            Term::from_field_text(self.schema.get_field(ITEM_BODY)?, search),
-            1,
-            true,
-        ));
+        let parsed_query = self.query_parser()?.parse_query(search)?;
 
-        let term = Box::new(TermQuery::new(
-            Term::from_field_text(self.schema.get_field(ITEM_BODY)?, search),
-            IndexRecordOption::Basic,
-        ));
-
-        let combined_query = BooleanQuery::new(vec![
-            (Occur::Must, story_term),
-            (
-                Occur::Must,
-                Box::new(BooleanQuery::new(vec![
-                    (Occur::Should, term),
-                    (Occur::Should, fuzzy_term),
-                ])),
-            ),
-        ]);
+        let combined_query =
+            BooleanQuery::new(vec![(Occur::Must, story_term), (Occur::Must, parsed_query)]);
 
         self.top_comments_with_count(limit, offset, combined_query)
     }
