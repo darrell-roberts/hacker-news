c849b6dcdbd64c1df09968436939482718dc4dd4
Darrell Roberts <droberts0674@gmail.com>
2024-12-24 14:17:27 -0500

    add warn for any unparsed html

--- html-sanitizer/src/lib.rs
+++ html-sanitizer/src/lib.rs
@@ -1,4 +1,4 @@
-use log::error;
+use log::{error, warn};
 use nom::error::VerboseError;
 
 mod parser;
@@ -8,6 +8,11 @@ pub use parser::{Anchor, Element};
 /// Parse the input str into elements.
 pub fn parse_elements(input: &str) -> Vec<Element> {
     parser::parse_nodes::<VerboseError<&str>>(input)
+        .inspect(|(rest, _)| {
+            if !rest.is_empty() {
+                warn!("Unparsed text left over: \"{rest}\"")
+            }
+        })
         .map(|(_, v)| v)
         .unwrap_or_else(|err| {
             error!("Failed to parse input: {err}");
