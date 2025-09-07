3b062fd5860dcdfec14f67090614ec5d40a41371
Darrell Roberts <droberts0674@gmail.com>
2025-09-07 17:55:25 -0400

    used SharedString for header

--- hacker-news-gpui/src/header.rs
+++ hacker-news-gpui/src/header.rs
@@ -2,33 +2,41 @@
 use crate::ArticleSelection;
 use gpui::{
     black, div, prelude::FluentBuilder, px, yellow, App, AppContext as _, BorrowAppContext, Entity,
-    InteractiveElement, IntoElement, MouseButton, ParentElement, Render, Styled, Window,
+    InteractiveElement, IntoElement, MouseButton, ParentElement, Render, SharedString, Styled,
+    Window,
 };
 use hacker_news_api::ArticleType;
 
 /// Header view
-pub struct Header;
+pub struct Header {
+    counts: [(usize, SharedString); 5],
+}
 
 impl Header {
     /// Create a new header view.
     pub fn new(_cx: &mut Window, app: &mut App) -> Entity<Self> {
-        app.new(|_cx| Self {})
+        app.new(|_cx| Self {
+            counts: [25, 50, 75, 100, 500].map(|n| (n, format!("{n}").into())),
+        })
     }
 }
 
 impl Render for Header {
     fn render(&mut self, _window: &mut Window, cx: &mut gpui::Context<Self>) -> impl IntoElement {
-        let app_state = cx.global::<ArticleSelection>();
+        let article_selection = cx.global::<ArticleSelection>();
 
         let mk_article_type = |article_type: ArticleType| {
             div()
-                .when(article_type == app_state.viewing_article_type, |div| {
-                    div.text_bg(yellow())
-                        .border_1()
-                        .text_color(black())
-                        .rounded(px(0.8))
-                })
-                .child(article_type.as_str().to_owned())
+                .when(
+                    article_type == article_selection.viewing_article_type,
+                    |div| {
+                        div.text_bg(yellow())
+                            .border_1()
+                            .text_color(black())
+                            .rounded(px(0.8))
+                    },
+                )
+                .child(article_type.as_str())
                 .on_mouse_down(MouseButton::Left, move |_event, _window, cx| {
                     cx.update_global(|state: &mut ArticleSelection, _cx| {
                         state.viewing_article_type = article_type;
@@ -44,21 +52,28 @@ impl Render for Header {
             .into_iter()
             .map(mk_article_type);
 
-        let col3 = [25, 50, 75, 100, 500].into_iter().map(|article_count| {
-            div()
-                .when(article_count == app_state.viewing_article_total, |div| {
-                    div.text_bg(yellow())
-                        .border_1()
-                        .text_color(black())
-                        .rounded(px(0.8))
-                })
-                .child(format!("{article_count}"))
-                .on_mouse_down(MouseButton::Left, move |_event, _window, cx| {
-                    cx.update_global(|state: &mut ArticleSelection, _cx| {
-                        state.viewing_article_total = article_count;
+        let col3 = self
+            .counts
+            .clone()
+            .into_iter()
+            .map(|(article_count, label)| {
+                div()
+                    .when(
+                        article_count == article_selection.viewing_article_total,
+                        |div| {
+                            div.text_bg(yellow())
+                                .border_1()
+                                .text_color(black())
+                                .rounded(px(0.8))
+                        },
+                    )
+                    .child(label)
+                    .on_mouse_down(MouseButton::Left, move |_event, _window, cx| {
+                        cx.update_global(|state: &mut ArticleSelection, _cx| {
+                            state.viewing_article_total = article_count;
+                        })
                     })
-                })
-        });
+            });
 
         div()
             .flex()
