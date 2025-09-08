9dbbb1e28988b3581958f2f355128d501caea888
Darrell Roberts <droberts0674@gmail.com>
2025-09-08 15:21:17 -0400

    show number of ranks changed and color for > 2 rank changes

--- hacker-news-gpui/src/article.rs
+++ hacker-news-gpui/src/article.rs
@@ -1,6 +1,4 @@
 //! Article view.
-use std::cmp::Ordering;
-
 use crate::UrlHover;
 use gpui::{
     black, div, prelude::*, px, rems, rgb, solid_background, white, AppContext, AsyncApp, Entity,
@@ -15,27 +13,27 @@ pub struct ArticleView {
     score: SharedString,
     comments: SharedString,
     url: Option<SharedString>,
-    order_change: SharedString,
+    order_change_label: SharedString,
+    order_change: i64,
 }
 
 impl ArticleView {
-    pub fn new(
-        app: &mut AsyncApp,
-        item: Item,
-        order_change: Ordering,
-    ) -> anyhow::Result<Entity<Self>> {
+    pub fn new(app: &mut AsyncApp, item: Item, order_change: i64) -> anyhow::Result<Entity<Self>> {
         app.new(|_| Self {
             title: item.title.unwrap_or_default().into(),
             author: format!("by {}", item.by.clone()).into(),
             score: format!("{}", item.score,).into(),
             comments: format!("💬{}", item.kids.len()).into(),
             url: item.url.map(Into::into),
-            order_change: match order_change {
-                Ordering::Less => "-",
-                Ordering::Equal => "",
-                Ordering::Greater => "+",
+            order_change_label: if order_change == 0 {
+                String::new()
+            } else if order_change.is_positive() {
+                format!("+{order_change}")
+            } else {
+                format!("{order_change}")
             }
             .into(),
+            order_change,
         })
     }
 }
@@ -48,9 +46,9 @@ impl Render for ArticleView {
     ) -> impl gpui::IntoElement {
         let rank_change_col = div()
             .flex()
-            .w(rems(1.0))
-            .justify_start()
-            .child(self.order_change.clone());
+            .w(rems(1.5))
+            .justify_end()
+            .child(self.order_change_label.clone());
 
         let points_col = div()
             .flex()
@@ -90,8 +88,10 @@ impl Render for ArticleView {
                                     app.open_url(url.as_ref());
                                 }
                             })
-                            .on_hover(move |_hover, _window, app| {
-                                if let Some(entity) = weak_entity.upgrade() {
+                            .on_hover(move |hover, _window, app| {
+                                if !hover {
+                                    app.set_global::<UrlHover>(UrlHover(None));
+                                } else if let Some(entity) = weak_entity.upgrade() {
                                     let view = entity.read(app);
                                     app.set_global::<UrlHover>(UrlHover(view.url.clone()));
                                 }
@@ -113,6 +113,8 @@ impl Render for ArticleView {
             .font_family("Roboto, sans-serif")
             .text_size(px(15.0))
             .text_color(white())
+            .when(self.order_change > 2, |div| div.text_color(rgb(0x66ff1a)))
+            .when(self.order_change < -2, |div| div.text_color(rgb(0xff1a1a)))
             .w_full()
             .gap_x(px(5.0))
             .child(rank_change_col)
