dcb5252215f4fc954851c967641c72073fc94e0f
Darrell Roberts <droberts0674@gmail.com>
2025-09-09 10:59:11 -0400

    update article layout

--- hacker-news-gpui/src/article.rs
+++ hacker-news-gpui/src/article.rs
@@ -2,8 +2,8 @@
 use crate::UrlHover;
 use chrono::{DateTime, Utc};
 use gpui::{
-    black, div, prelude::*, px, rems, rgb, solid_background, white, AppContext, AsyncApp, Entity,
-    Fill, FontWeight, SharedString, Window,
+    black, div, img, prelude::*, px, rems, rgb, solid_background, white, AppContext, AsyncApp,
+    Entity, Fill, FontWeight, SharedString, Window,
 };
 use hacker_news_api::Item;
 
@@ -19,13 +19,15 @@ pub struct ArticleView {
     age: SharedString,
 }
 
+static COMMENT_SVG: &str = include_str!("../../assets/comment.svg");
+
 impl ArticleView {
     pub fn new(app: &mut AsyncApp, item: Item, order_change: i64) -> anyhow::Result<Entity<Self>> {
         app.new(|_| Self {
             title: item.title.unwrap_or_default().into(),
             author: format!("by {}", item.by.clone()).into(),
             score: format!("{}", item.score,).into(),
-            comments: format!("💬{}", item.kids.len()).into(),
+            comments: format!("{}", item.kids.len()).into(),
             url: item.url.map(Into::into),
             order_change_label: if order_change == 0 {
                 String::new()
@@ -62,21 +64,7 @@ impl Render for ArticleView {
         let comments_col = div()
             .flex()
             .w(rems(3.))
-            .justify_start()
-            .child(self.comments.clone());
-
-        let author = div()
-            .italic()
-            .font_family(SharedString::new_static(".SystemUIFont"))
-            .text_size(px(12.0))
-            // .justify_end()
-            .child(self.author.clone());
-
-        let age = div()
-            .italic()
-            .font_family(SharedString::new_static(".SystemUIFont"))
-            .text_size(px(12.0))
-            .child(self.age.clone());
+            .child(div().child(img(COMMENT_SVG)).child(self.comments.clone()));
 
         let url = self.url.clone();
 
@@ -113,8 +101,18 @@ impl Render for ArticleView {
                             .bg(Fill::Color(solid_background(rgb(0x00134d))))
                     }),
             )
-            .child(author)
-            .child(age)
+            .child(
+                div()
+                    .flex()
+                    .flex_row()
+                    .italic()
+                    .items_center()
+                    .font_family(SharedString::new_static(".SystemUIFont"))
+                    .text_size(px(12.0))
+                    .child(self.author.clone())
+                    .child(self.age.clone())
+                    .gap_x(px(5.0)),
+            )
             .gap_x(px(5.0));
 
         div()
@@ -124,7 +122,7 @@ impl Render for ArticleView {
             .text_size(px(15.0))
             .text_color(white())
             .when(self.order_change > 2, |div| div.text_color(rgb(0x66ff1a)))
-            .when(self.order_change < -2, |div| div.text_color(rgb(0xff1a1a)))
+            .when(self.order_change < -2, |div| div.text_color(rgb(0xff99c2)))
             .w_full()
             .gap_x(px(5.0))
             .child(rank_change_col)
