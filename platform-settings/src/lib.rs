1fad0913cb5e049bb694d8d415bca5588d9b81f9
Darrell Roberts <droberts0674@gmail.com>
2025-09-14 11:39:49 -0400

    update platform-settings

--- platform-settings/src/lib.rs
+++ platform-settings/src/lib.rs
@@ -6,8 +6,20 @@ mod macos;
 #[cfg(target_os = "linux")]
 pub use linux::*;
 
+/// A change to the system theme
 #[derive(Debug, Copy, Clone)]
 pub enum Theme {
+    /// Dark mode enabled
     Dark,
+    /// Light mode enabled
     Light,
+}
+
+/// System setting change.
+#[derive(Debug, Copy, Clone)]
+pub enum SettingChange {
+    /// System theme changed
+    Theme(Theme),
+    /// System font scale changed
+    FontScale(f64),
 }
